8ce2fe12b44be0530fd2e2e6c54b403fc3040021
Scott Shie <sshie@aerisweather.com>
2017-08-23 10:53:27 -0500

    Updated sunmoon view. Updated storm cell colors.

--- AerisDemo/src/com/example/customendpoint/CustomSunmoonHolder.java
+++ AerisDemo/src/com/example/customendpoint/CustomSunmoonHolder.java
@@ -33,29 +33,45 @@ public class CustomSunmoonHolder implements AdapterHolder<CustomSunmoonModel> {
 	}
 
 	@Override
-	public void populateView(CustomSunmoonModel model, int position) {
-		sunriseTextView.setText(WeatherUtil.getFormatFromISO(model.sun.riseISO,
-				"h:mm a"));
-		sunsetTextView.setText(WeatherUtil.getFormatFromISO(model.sun.setISO,
-				"h:mm a"));
-		if (position == 0) {
+	public void populateView(CustomSunmoonModel model, int position)
+	{
+		if (model.sun.riseISO != null)
+			sunriseTextView.setText(WeatherUtil.getFormatFromISO(model.sun.riseISO, "h:mm a"));
+		else
+			sunriseTextView.setText("N/A");
+
+		if (model.sun.setISO != null)
+			sunsetTextView.setText(WeatherUtil.getFormatFromISO(model.sun.setISO, "h:mm a"));
+		else
+			sunsetTextView.setText("N/A");
+
+		if (position == 0)
+		{
 			titleView.setText("Today");
-		} else if (position == 1) {
+		}
+		else if (position == 1)
+		{
 			titleView.setText("Tomorrow");
-		} else {
-			titleView.setText(WeatherUtil.getFormatFromISO(model.dateTimeISO,
-					"EEE dd"));
+		}
+		else
+		{
+			titleView.setText(WeatherUtil.getFormatFromISO(model.dateTimeISO, "EEE dd"));
 		}
 
-		long seconds = model.sun.set - model.sun.rise;
-		long minutes = seconds % 60;
-		long hours = seconds / 60 / 60;
+		if((model.sun.riseISO != null) && (model.sun.setISO != null))
+		{
+			long seconds = model.sun.set - model.sun.rise;
+			long hours = seconds / 60 / 60;
+			long minutes = (seconds / 60) - (hours * 60);
 
-		totalDaylightTextView.setText(String.format("%d hours,%d minutes",
-				hours, minutes));
-		totalDaylightPBar.setProgress((int) seconds);
-		moonphaseTextView
-				.setText(WeatherUtil.capitalize(model.moon.phase.name));
+			totalDaylightTextView.setText(String.format("%d hours,%d minutes", hours, minutes));
+			totalDaylightPBar.setProgress((int) seconds);
+		}
+		else
+		{
+			totalDaylightTextView.setText("N/A");
+		}
+
+		moonphaseTextView.setText(WeatherUtil.capitalize(model.moon.phase.name));
 	}
-
 }
